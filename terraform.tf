a0ead0dc41b194d86a4cf4f20a52eea844de5e6b
acauret <andrew.auret@alikeconsultancy.co.uk>
2024-09-24 14:59:37 +0100

    Update azurerm and azapi providers to latest versions

--- terraform.tf
+++ terraform.tf
@@ -3,12 +3,11 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = "1.9.0, < 2.0.0"
+      version = ">= 1.14.0, < 2.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 4.0.0"
+      version = ">= 3.114.0, < 4.0.0"
     }
-
   }
 }
